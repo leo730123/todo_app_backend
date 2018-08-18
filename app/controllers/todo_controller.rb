f4e30e8155aff5cd4db81b2ed9846e671fd3e211
leo <leohuang213@gmail.com>
2018-08-18 20:50:00 +0000

    Ruby Link

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -3,7 +3,7 @@ class TodoController <ApplicationController
     end  
     
     def show
-        @todo_description="Create and finish the curriculum. "
+        @todo_description="Create and finish the curriculum for Ethnic and ELA Class"
         @todo_pomodoro_estimate = 4
     end    
 end
