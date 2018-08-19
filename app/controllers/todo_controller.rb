ae9ffff90929e4da72c0b65b8c52d144ca5cda50
leo <leohuang213@gmail.com>
2018-08-19 17:26:50 +0000

    Database adding

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -3,27 +3,7 @@ class TodoController <ApplicationController
     end  
     
     def show
-        if params[:id]=="1"
-            @todo_description="Make the curriculum"
-            @todo_pomodoro_estimate=4
-        elsif params[:id]=="2"
-            @todo_description="Buy the workshop supplies"
-            @todo_pomodoro_estimate=3
-        elsif params[:id]=="3"
-            @todo_description="Meet with volunteer trainers"
-            @todo_pomodoro_estimate=2
-        elsif params[:id]=="4"   
-            @todo_description="Order food for Saturday and Sunday"
-            @todo_pomodoro_estimate=1
-        elsif params[:id]=="5"
-            @todo_description="Check pre-work assignments"
-            @todo_pomodoro_estimate=="2"
-        elsif params[:id]=="6"   
-            @todo_description="Send workshop location to the students"
-            @todo_pomodoro_estimate=="1"
-        elsif params[:id]=="7"    
-            @todo_description="Have a great workshop"
-            @todo_pomodoro_estimate=="0"
-        end
+        @t=Todo.find(params[:id])
+        
     end    
 end
